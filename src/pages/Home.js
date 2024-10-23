c3b299f812ccb990c05a35580772258350d308ca
Edidiong Udoh <gisthive@gmail.com>
2024-10-23 15:27:33 +0100

    added card to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,9 @@
+import Card from "../components/Card";
+
 export function Home() {
-    return <div>My Home Page</div>;
+    return <div><Card image="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/31/6704752/1.jpg?2823" caption="Ace Electric"></Card>
+        <Card image="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/88/1724373/1.jpg?0045" caption=" Syinix Electric Kettle"></Card>
+        <Card image="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/96/0977963/1.jpg?7179" caption="Tecno Spark 30"></Card>
+        <Card image="https://ng.jumia.is/cms/0-5-brand-festival/2024/Collection/homepage-thumbnails/renewed.jpg" caption="Phone"></Card>
+        <Card image="https://ng.jumia.is/cms/0-5-brand-festival/2024/Collection/homepage-thumbnails/special-offer.png" caption="Special Offer"></Card> </div>;
 }
